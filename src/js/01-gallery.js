86bdf39e8e6f41424faff65488554bbe8ebe5ad4
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2024-01-26 00:16:11 +0200

    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', onClickModal);
+
+const alleryItemList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', alleryItemList);
+
+function onClickModal(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('gallery__image')) {
+    const largeImg = e.target.dataset.source;
+
+  }
+}
+
+
+
+// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько
+//  подзадач:
+// Создание и рендер разметки по массиву данных
+// galleryItems и предоставленному шаблону элементагалереи.
+// Реализация делегирования на ul.gallery и
+// получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна
+// basicLightbox. Используй CDN сервис jsdelivr и добавь
+//  в проект ссылки на минифицированные (.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи.
+//  Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента <img>
+// в модальном окне перед открытием.
+// Используй готовую разметку модального окна с изображением из
+// примеров библиотеки basicLightbox.
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js.
+// Разбей его на несколько подзадач:
+// 1Создание и рендер разметки по массиву данных galleryItems и
+// предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+// 2Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs.
+//  Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+// 3Инициализация библиотеки после того как элементы галереи созданы и
+// добавлены в div.gallery. Для этого ознакомься с документацией SimpleLightbox
+// - в первую очередь секции «Usage» и «Markup».
+// 4Посмотри в документации секцию «Options» и добавь отображение подписей
+// к изображениям из атрибута alt. Пусть подпись будет снизу и появляется
+// через 250 миллисекунд после открытия изображения.
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', onClickModal);
+
+const alleryItemList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', alleryItemList);
+
+
+function onClickModal(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('gallery__image')) {
+    const largeImg = e.target.dataset.source;
+  }
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
